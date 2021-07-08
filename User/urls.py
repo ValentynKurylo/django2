2f31b5bd069627942ee8b8ad965f2386e0459e41
ValentynKurylo <56786816+ValentynKurylo@users.noreply.github.com>
2021-07-08 16:22:35 +0300

    django2

--- User/urls.py
+++ User/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import show_user, new_user
+from .views import show_user, new_user, delete_user
 
 urlpatterns = [
     path('', show_user),
-    path('/create/<str:name>/<int:age>', new_user)
+    path('/create/<str:name>/<int:age>/<int:id>', new_user),
+    path('/delete/<int:id_user>', delete_user)
 ]
--- User/views.py
+++ User/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 
 user = [
-    {'name': 'Valentyn', 'age': 19}
+    {'id': 0, 'name': 'Valentyn', 'age': 19},
+    {'id': 1, 'name': 'Maria', 'age': 20}
 ]
-
-
 def show_user(request):
     return render(request, 'user.html', {'user': user})
 
 
-def new_user(request, name, age):
-    user.append({'name': name, 'age': age})
+
+def new_user(request, name, age, id):
+    user.append({'id': id, 'name': name, 'age': age})
+    return render(request, 'user.html', {'user': user})
+
+def delete_user(request, id_user):
+    for i in user:
+        if i.id == id_user:
+            del i
     return render(request, 'user.html', {'user': user})
